30b5b324120e65036c42502ab28f7519e9c5e75f
Daryu-Kim <dev_daryu@kakao.com>
2025-07-13 02:53:47 +0900

    20250713_0253

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "네코쿠모 - 프리미엄 전자담배 쇼핑몰"
+    }
+  }
 });

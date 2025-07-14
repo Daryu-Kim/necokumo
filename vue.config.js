57170a4797ef08adccceede163ee26dcd0687fde
Daryu-Kim <dev_daryu@kakao.com>
2025-07-14 15:31:49 +0900

    20250714_1531

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,14 @@ module.exports = defineConfig({
   pages: {
     index: {
       entry: "src/main.js",
-      title: "네코쿠모 - 프리미엄 전자담배 쇼핑몰"
-    }
-  }
+      title: "네코쿠모 - 프리미엄 전자담배 쇼핑몰",
+    },
+  },
+  pwa: {
+    name: "네코쿠모",
+    themeColor: "#ffffff",
+    backgroundColor: "#ffffff",
+    display: "standalone",
+    start_url: "/",
+  },
 });
